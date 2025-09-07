45bbae75432e5fdd6f328588884d95eaec47c416
Huzaifa Haris <khuzaifa743@gmail.com>
2025-09-07 23:45:32 +0500

    test

---
+++ month-first/doms/day-2/main.js
@@ -0,0 +1,11 @@
+let paragraph = document.getElementById("para")
+paragraph.textContent = "hello amna"
+
+
+let box = document.getElementById("box")
+
+button = document.getElementById("button")
+
+function button(){
+    box.classList.toggle('round')
+}
